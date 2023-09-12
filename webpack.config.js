8ae186752fb52242592711692f707ed1ae7d354d
aadamliu <aadamliu@tencent.com>
2023-09-12 19:35:41 +0800

    feat(a): 压缩体积

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,9 @@ module.exports = function () {
   }
 
   return {
+    devServer: {
+      port: 3000,
+    },
     entry: {
       'site-mobile': ['./docs/site/entry'],
       'site-desktop': ['./docs/site/entry'],
